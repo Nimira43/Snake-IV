97963ee7be0b8900727df8a5ac3128e4d17c556d
Nimira43 <n.rabone72@aol.com>
2024-08-29 20:53:08 +0100

    Starting to set up fully responsive canvas

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
+class Game {
+  constructor(canvas, context) {
+    this.canvas = canvas
+    this.ctx = context
+    this.width = this.canvas.width
+    this.height = this.canvas.height
+  
+    window.addEventListener('resize', e => {
+      this.canvas.width = e.currentTarget.innerWidth
+    })
+  }
+  resize(width, height)
+}
+
 window.addEventListener('load', function () {
   const canvas = document.getElementById('canvas1')
   const ctx = canvas.getContext('2d')
-  canvas.width = 1500
-  canvas.height = 500
+  canvas.width = window.innerWidth
+  canvas.height = window.innerHeight;
   
+  const game = new Game(canvas, ctx)
+
 })
