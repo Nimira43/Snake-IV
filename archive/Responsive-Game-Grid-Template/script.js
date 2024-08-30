d7d015ebbe8e4d2eda620a1f16aad2aa9c72776e
Nimira43 <n.rabone72@aol.com>
2024-08-30 21:03:54 +0100

    Created archive folder. Made a copy of the code so far  called Responsive Game Grid Template for possible use for other projects in the future.

---
+++ archive/Responsive-Game-Grid-Template/script.js
@@ -0,0 +1,61 @@
+class Game {
+  constructor(canvas, context) {
+    this.canvas = canvas
+    this.ctx = context
+    this.width
+    this.height
+    this.snake = new Snake(this, 0, 0, 0, 1)
+    this.cellSize = 50
+    this.columns
+    this.rows
+
+    window.addEventListener('resize', e => {
+      this.resize(e.currentTarget.innerWidth, e.currentTarget.innerHeight)
+    })
+    this.resize(window.innerWidth, window.innerHeight)
+  }
+  resize(width, height) {
+    this.canvas.width = width - width % this.cellSize
+    this.canvas.height = height - height % this.cellSize
+    this.ctx.fillStyle = '#ff4500'
+    this.width = this.canvas.width
+    this.height = this.canvas.height
+    this.columns = Math.floor(this.width / this.cellSize)
+    this.rows = Math.floor(this.height / this.cellSize)
+    this.render()
+  }
+  drawGrid() {
+    for (let y = 0; y < this.rows; y++) {
+      for (let x = 0; x < this.columns; x++) {
+        this.ctx.strokeRect(
+          x * this.cellSize,
+          y * this.cellSize,
+          this.cellSize,
+          this.cellSize)
+      }
+    }
+  }
+
+  render() {
+    this.drawGrid()
+    // this.snake.update()
+    // this.snake.draw()
+  }
+}
+
+window.addEventListener('load', function () {
+  const canvas = document.getElementById('canvas1')
+  const ctx = canvas.getContext('2d')
+  canvas.width = window.innerWidth
+  canvas.height = window.innerHeight;
+  
+  const game = new Game(canvas, ctx)
+  game.render()
+
+  function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    game.render()
+    requestAnimationFrame(animate)
+  }
+  requestAnimationFrame(animate)
+})
