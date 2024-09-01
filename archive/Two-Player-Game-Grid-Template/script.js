dc404f1594ed2038e4867e88fc88aca64449583c
Nimira43 <n.rabone72@aol.com>
2024-09-02 00:30:20 +0100

    Added to Archive the basic template for a working two player Game Grid

---
+++ archive/Two-Player-Game-Grid-Template/script.js
@@ -0,0 +1,90 @@
+class Game {
+  constructor(canvas, context) {
+    this.canvas = canvas
+    this.ctx = context
+    this.width
+    this.height
+    this.cellSize = 50
+    this.columns
+    this.rows
+  
+    this.eventTimer = 0
+    this.eventInteval = 200
+    this.eventUpdate = false
+  
+    this.player1 = new Keyboard1(this, 0, 0, 0, 1, '#ff4500')
+    this.player2 = new Keyboard2(this, 5, 0, 0, 1, '#87ceeb')
+    this.gameObjects = [this.player1, this.player2]
+    
+    window.addEventListener('resize', e => {
+      this.resize(e.currentTarget.innerWidth, e.currentTarget.innerHeight)
+    })
+    this.resize(window.innerWidth, window.innerHeight)
+  }
+
+  resize(width, height) {
+    this.canvas.width = width - width % this.cellSize
+    this.canvas.height = height - height % this.cellSize
+    this.ctx.fillStyle = '#ff4500'
+    this.width = this.canvas.width
+    this.height = this.canvas.height
+    this.columns = Math.floor(this.width / this.cellSize)
+    this.rows = Math.floor(this.height / this.cellSize)
+  }
+
+  drawGrid() {
+    for (let y = 0; y < this.rows; y++) {
+      for (let x = 0; x < this.columns; x++) {
+        this.ctx.strokeRect(
+          x * this.cellSize,
+          y * this.cellSize,
+          this.cellSize,
+          this.cellSize
+        )
+      }
+    }
+  }
+
+  handlePeriodicEvents(deltaTime) {
+    if (this.eventTimer < this.eventInteval) {
+      this.eventTimer += deltaTime
+      this.eventUpdate = false
+    } else {
+      this.eventTimer = 0
+      this.eventUpdate = true
+    }
+  }
+
+  render(deltaTime) {
+    this.handlePeriodicEvents(deltaTime)
+    if (this.eventUpdate) {
+      this.ctx.clearRect(0, 0, this.width, this.height)
+      this.drawGrid()
+      this.gameObjects.forEach(object => {
+        object.draw()
+        object.update()
+      })
+    }
+  }
+}
+
+window.addEventListener('load', function () {
+  const canvas = document.getElementById('canvas1')
+  const ctx = canvas.getContext('2d')
+  canvas.width = window.innerWidth
+  canvas.height = window.innerHeight;
+  
+  const game = new Game(canvas, ctx)
+  game.render()
+
+  let lastTime = 0
+
+  function animate(timeStamp) {
+    const deltaTime = timeStamp - lastTime
+    lastTime = timeStamp
+    
+    game.render(deltaTime)
+    requestAnimationFrame(animate)
+  }
+  requestAnimationFrame(animate)
+})
---
+++ archive/Two-Player-Game-Grid-Template/snake.js
@@ -0,0 +1,84 @@
+class Snake {
+  constructor(game, x, y, speedX, speedY, colour) {
+    this.game = game
+    this.x = x
+    this.y = y
+    this.speedX = speedX
+    this.speedY = speedY
+    this.colour = colour
+    this.width = this.game.cellSize
+    this.height = this.game.cellSize
+    this.moving = true
+  }
+  
+  update() {
+    if (this.x <= 0 && this.speedX < 0 ||
+        this.x >= this.game.columns - 1 && this.speedX > 0 ||
+        this.y <= 0 && this.speedY < 0 ||
+        this.y >= this.game.rows - 1 && this.speedY > 0) {
+      this.moving = false
+    }
+    if (this.moving) {
+      this.x += this.speedX
+      this.y += this.speedY
+    }
+  }
+  
+  draw() {
+    this.game.ctx.fillStyle = this.colour
+    this.game.ctx.fillRect(
+      this.x * this.game.cellSize,
+      this.y * this.game.cellSize,
+      this.width,
+      this.height
+    )
+  }
+  
+  turnUp() {
+    this.speedX = 0
+    this.speedY = -1
+    this.moving = true
+  }
+  
+  turnDown() {
+    this.speedX = 0
+    this.speedY = 1
+    this.moving = true
+  }
+  turnLeft() {
+    this.speedX = -1
+    this.speedY = 0
+    this.moving = true
+  }
+  turnRight() {
+    this.speedX = 1
+    this.speedY = 0
+    this.moving = true
+  }
+}
+
+class Keyboard1 extends Snake {
+  constructor(game, x, y, speedX, speedY, colour) {
+    super(game, x, y, speedX, speedY, colour)
+    
+    window.addEventListener('keydown', e => {
+      if (e.key === 'ArrowUp') this.turnUp()
+      else if (e.key === 'ArrowDown') this.turnDown()
+      else if (e.key === 'ArrowLeft') this.turnLeft()
+      else if (e.key === 'ArrowRight') this.turnRight()
+    })
+  }
+}
+
+class Keyboard2 extends Snake {
+  constructor(game, x, y, speedX, speedY, colour) {
+    super(game, x, y, speedX, speedY, colour)
+    
+    window.addEventListener('keydown', e => {
+      if (e.key.toLowerCase() === 'q') this.turnUp()
+      else if (e.key.toLowerCase() === 'a') this.turnDown()
+      else if (e.key.toLowerCase() === 'r') this.turnLeft()
+      else if (e.key.toLowerCase() === 't') this.turnRight()
+    })
+  }
+}
