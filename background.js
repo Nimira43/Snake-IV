0218b3df3e6d3c127c24588215903084db5fb886
Nimira43 <n.rabone72@aol.com>
2025-07-12 17:01:37 +0100

    Update Background constructor

--- background.js
+++ background.js
@@ -4,7 +4,11 @@ class Background {
     this.x = 0;
     this.y = 0;
     this.width = this.game.cellSize * 8
-    this.height = this.game.cellSize * this.game.topMargin + 1
+    this.height = this.game.cellSize * (this.game.topMargin + 1)
+    this.image = document.getElementById('forest_margin_repeat')
+    this.repeats = this.game.width / this.width
   }
-  draw()
+  draw() {
+    
+  }
 }
