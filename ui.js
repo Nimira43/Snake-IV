675024fae6f53a0eb38fb0ed45028920b7494f80
Nimira43 <n.rabone72@aol.com>
2025-07-12 17:22:43 +0100

    Updated ui.ks

--- ui.js
+++ ui.js
@@ -12,10 +12,29 @@ class Ui {
       this.game.start()
     })
   }
+
   update() {
     this.scoreBoard1.innerText = this.game.player1.name + ' - ' + this.game.player1.score 
     this.scoreBoard2.innerText = this.game.player2.name + ' - ' + this.game.player2.score 
     this.scoreBoard3.innerText = this.game.player3.name + ' - ' + this.game.player3.score 
     this.scoreBoard4.innerText = this.game.player4.name + ' - ' + this.game.player4.score 
   }
+
+  triggerGameOver() {
+    this.game.gameOver = true
+    this.gameOverUi()
+  }
+
+  gameplayUi() {
+    this.gameMenu.style.display = 'none'
+    this.startButton.innerText = 'Restart'
+    this.gameOverScreen.style.display = 'none'
+  }
+
+  gameOverUi() {
+    this.gameMenu.style.display ='block'
+    this.startButton.innerText = 'Start'
+    this.gameOverScreen.style.display = 'block'
+  }
+
 }
