27161b6fde65ef0552c7f2cd06572977ab7d270a
Nimira43 <n.rabone72@aol.com>
2025-08-28 02:19:27 +0100

    Particle constructor

--- particles.js
+++ particles.js
@@ -1,5 +1,17 @@
 class Particle {
-  constructor(game){}
+  constructor(game){
+    this.game = game
+    this.x = 0
+    this.y = 0
+    this.radius
+    this.speedX = 1
+    this.speedY = 1
+    this.colour
+    this.angle
+    this.va = Math.random() * 0.2 - 0.1
+    this.free = true
+  }
+
   reset(){}
   start(x, y, colour){}
   update(){}
