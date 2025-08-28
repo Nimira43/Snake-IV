00c97f01eca3bf77c5ecf8c0978ac79803fd271d
Nimira43 <n.rabone72@aol.com>
2025-08-28 18:36:45 +0100

    AudioControl constructor - 1

--- sounds.js
+++ sounds.js
@@ -1,4 +1,11 @@
 class AudioControl {
-  constructor(){}
+  constructor(){
+    this.bite1 = document.getElementById('bite1')
+    this.bite2 = document.getElementById('bite2')
+    this.bite3 = document.getElementById('bite3')
+    this.bite4 = document.getElementById('bite4')
+    this.bite5 = document.getElementById('bite5')
+    this.biteSounds = [this.bite1, this.bite2,this.bite3, this.bite4, this.bite5]
+  }
   play(sound){}
 }
