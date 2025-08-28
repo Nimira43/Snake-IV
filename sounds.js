58a31e9b5d4db45de17b109339fcd739e68a6ba6
Nimira43 <n.rabone72@aol.com>
2025-08-28 18:38:48 +0100

    AudiControl - 2

--- sounds.js
+++ sounds.js
@@ -6,6 +6,11 @@ class AudioControl {
     this.bite4 = document.getElementById('bite4')
     this.bite5 = document.getElementById('bite5')
     this.biteSounds = [this.bite1, this.bite2,this.bite3, this.bite4, this.bite5]
+    this.bad_food = document.getElementById('bad_food')
+    this.start = document.getElementById('start')
+    this.restart = document.getElementById('restart')
+    this.win = document.getElementById('win')
+    this.button = document.getElementById('button')
   }
   play(sound){}
 }
