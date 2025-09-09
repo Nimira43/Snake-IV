c4f3fa1f0ff77f9be5785914cf3a2596b86e7ea0
Nimira43 <n.rabone72@aol.com>
2025-09-09 22:20:48 +0100

    handleParticles method updated

--- script.js
+++ script.js
@@ -167,8 +167,14 @@ class Game {
     }
   }
 
+  handleParticle() {
+    this.ctx2.clearRect(0, 0, this.width, this.height) 
 
-  handleParticle() {}
+    for (let i = 0; i < this.particles.length; i++) {
+      this.particles[i].update()
+      this.particles[i].draw()
+    }
+  }
 
   render(deltaTime) {
     this.handlePeriodicEvents(deltaTime)
